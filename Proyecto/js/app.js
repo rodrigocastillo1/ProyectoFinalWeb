8fdabd67dd1d7cf2b7de10e7dc433b8d2c1bfbc1
Rodrigo_Castillo <castillo_15zs@hotmail.com>
2017-04-30 18:49:53 -0500

    Se agrega la estructura de proyecto con los frameworks a utilizar y se añade el archivo index.html donde se crea la barra de navegación. Faltan detalles como centrar sus elementos y terminar de dar estilo al footer. Pero ya me voy a comer :), regreso

---
+++ Proyecto/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.parallax').parallax();
+});
+
+$(document).ready(function(){
+	$('.carousel').carousel();
+});
+
+$(document).ready(function(){
+	$('.modal').modal();
+});
