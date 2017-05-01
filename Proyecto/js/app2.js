5dd0b69b1214680fc2acc4e141e3400d4ca0f8a1
Daniel1597 <danifanton_15@hotmail.com>
2017-05-01 08:09:03 -0500

    Se agregaron los cursos y la busqueda

---
+++ Proyecto/js/app2.js
@@ -0,0 +1,12 @@
+//Archivo de configuración de angular
+$(document).ready(function(){
+	$('.parallax').parallax();
+});
+
+$(document).ready(function(){
+	$('.carousel').carousel();
+});
+
+$(document).ready(function(){
+	$('.modal').modal();
+});
